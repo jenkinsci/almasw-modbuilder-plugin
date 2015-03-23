99ea899472d107300ad67c9e43cb4a6a287a9a16
Alexis Tejeda <alexis.tejeda@gmail.com>
2015-03-23 17:07:15 -0600

    RuntimeConfiguration (static runtime variables) added

---
+++ src/main/java/jenkins/plugins/almasw/builder/RuntimeConfiguration.java
@@ -0,0 +1,8 @@
+package jenkins.plugins.almasw.builder;
+
+public class RuntimeConfiguration {
+	
+	public static final String PLUGIN_NAME = "almasw-modbuilder";
+	public static final String LOGGER_PREFIX  = "[ " + PLUGIN_NAME  + "]";
+
+}
